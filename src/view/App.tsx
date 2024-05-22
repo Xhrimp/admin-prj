8d149ade2f83875e8dcc60eea751bfc5fcfd4b08
Quangnguen <quanguyen3000k@gmail.com>
2024-05-22 19:44:43 +0700

    login

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,31 +1,29 @@
-import React from 'react';
-import './App.css';
-import logo from '../logo.svg'
-import Switch from '@mui/material/Switch';
-import { Button } from '@mui/material';
+import React from 'react'
+import './App.css'
+import { ThemeProvider, createTheme } from '@mui/material'
+import Login from './login'
 
-const label = { inputProps: { 'aria-label': 'Switch demo' } };
+const label = { inputProps: { 'aria-label': 'Switch demo' } }
+
+const theme = createTheme({
+  components: {
+    MuiStack: {
+      defaultProps: {
+        useFlexGap: true,
+        fontFamily: 'Roboto, sans-serif',
+      },
+    },
+  },
+})
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Switch {...label} defaultChecked />
-        <Button>abc</Button>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Login />
+      </div>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+export default App
