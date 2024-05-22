c34d167b1576df4bea84ef9b4ce65bf8e227a488
Nhanchinh <thannhanchinh2472003@gmail.com>
2024-05-22 19:46:12 +0700

    ci

---
+++ src/view/login/fogotPass/FogotPass.jsx
@@ -0,0 +1,25 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+function FogotPass() {
+
+    return (
+        <>
+            <div className="container">
+                <div className="header">
+                    <div className="header_title"><h2>Forgot your password</h2></div>
+                    <div className="header_sub_title"></div>
+                </div>
+
+
+            </div>
+
+
+        </>
+
+    )
+
+
+
+}
+export default FogotPass;
