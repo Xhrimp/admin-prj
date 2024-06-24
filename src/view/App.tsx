9899dcbb86d02992fa5d651acb2d53a9f6ec951e
Quangnguen <quanguyen3000k@gmail.com>
2024-06-24 18:00:10 +0700

    dfdfd

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -3,13 +3,14 @@ import logo from '../logo.svg'
 import SideBar from './common/sidebar'
 import React from 'react'
 import './App.css'
-import Schedule from './home/schedule/Calendar';
-import Routers from '../router/Routers';
+import Schedule from './home/schedule/Calendar'
+import Routers from '../router/Routers'
 import { ThemeProvider, createTheme } from '@mui/material'
 import Login from './login'
 import FogotPass from './login/fogotPass/FogotPass'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from './layout/header';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Header from './layout/header'
+import MyCourse from './home/mycourse/MyCourse'
 const label = { inputProps: { 'aria-label': 'Switch demo' } }
 
 const theme = createTheme({
@@ -26,12 +27,11 @@ const theme = createTheme({
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Header />} />
+      <Route path="/" element={<MyCourse />} />
       <Route path="/ForgotPass" element={<FogotPass />} />
       <Route path="/Login" element={<Login />} />
-
     </Routes>
-  );
+  )
 }
 
 export default App
